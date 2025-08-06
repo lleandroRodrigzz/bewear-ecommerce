7ae456ceb5cb55b2378c7cc53ac85feade0256ea
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2025-08-06 13:58:42 -0300

    Feat: Add sign in and sign out

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
@@ -5,20 +6,24 @@ import SignUpForm from "./components/sign-up-form";
 
 export function Authentication() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+
+      <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sign-in">
+            <SignInForm />
+          </TabsContent>
+          <TabsContent value="sign-up">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }
 
